cee21cdf07f085dd2692b679f0839075e08e227b
Jevans13 <Jack13evans@gmail.com>
2022-12-21 10:16:58 -0500

    Set CoreUnitAuditor to active

---
+++ migrations/new/202212211300_release_202212.js
@@ -0,0 +1,21 @@
+import knex from "knex";
+//Migration sets the Core Unit Auditor Role System value to true
+
+//Up migration creates the new table
+export async function up(knex) {
+
+    await knex('Role')
+    .where('roleName', 'CoreUnitAuditor')
+    .update({system: true})
+
+};
+
+
+//Down migration reverts the up migration change
+export async function down(knex) {
+
+    await knex('Role')
+    .where('roleName', 'CoreUnitAuditor')
+    .update({system: null})
+
+};
