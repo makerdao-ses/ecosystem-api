e16ade45552a57cf2c6e5c977184da66f14b5909
liberuum <liberom@proton.me>
2023-02-07 14:17:43 +0000

    added more stub data

--- src/modules/TotalExpenses/stubData.js
+++ src/modules/TotalExpenses/stubData.js
@@ -13,6 +13,27 @@ export const quarterlyExpenses = [
         budgetCap: 15132650.00,
     },
     {
+        period: "2022-Q1",
+        budget: "/makerdao/core-units",
+        prediction: 15512500.00,
+        actuals: 14465122.00,
+        budgetCap: 15132650.00,
+    },
+    {
+        period: "2022-Q2",
+        budget: "/makerdao/core-units",
+        prediction: 12512500.00,
+        actuals: 11465122.00,
+        budgetCap: 15132650.00,
+    },
+    {
+        period: "2022-Q3",
+        budget: "/makerdao/core-units",
+        prediction: 10512500.00,
+        actuals: 10465122.00,
+        budgetCap: 15132650.00,
+    },
+    {
         period: "2022-Q4",
         budget: "/makerdao/core-units",
         prediction: 13512500.00,
@@ -40,8 +61,31 @@ export const monthlyExpenses = [
         prediction: 13512500.00,
         actuals: 12465122.00,
         discontinued: 15132650.00,
-    },
-    {
+    }, {
+        period: "2022-08",
+        budget: "/makerdao/core-units",
+        prediction: 13912500.00,
+        actuals: 12965122.00,
+        discontinued: 14132650.00,
+    }, {
+        period: "2022-09",
+        budget: "/makerdao/core-units",
+        prediction: 12512500.00,
+        actuals: 11465122.00,
+        discontinued: 14132650.00,
+    }, {
+        period: "2022-10",
+        budget: "/makerdao/core-units",
+        prediction: 11512500.00,
+        actuals: 10465122.00,
+        discontinued: 13132650.00,
+    }, {
+        period: "2022-11",
+        budget: "/makerdao/core-units",
+        prediction: 10512500.00,
+        actuals: 10065122.00,
+        discontinued: 12032650.00,
+    }, {
         period: "2022-12",
         budget: "/makerdao/core-units",
         prediction: 13512500.00,
