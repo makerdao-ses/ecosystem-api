071f7bfd99b7f72ba027e60cfdb1b046022471dd
wkampmann <wouter@ksdc.be>
2023-02-19 18:47:55 +0100

    ResolverCache improvements + unit test.

--- src/modules/BudgetStatement/ResolverCache.ts
+++ src/modules/BudgetStatement/ResolverCache.ts
@@ -2,7 +2,6 @@ import { Knex } from "knex";
 import { BudgetReportOutputGroup, CacheKeys } from "./BudgetReportResolver.js";
 import stringify from "json-stringify-deterministic";
 import xxhash from "xxhash-wasm";
-import { reviver } from "./JsonSerializerTypes.js";
 
 const DEBUG_OUTPUT = false;
 
@@ -14,6 +13,22 @@ export class ResolverCache {
 
     constructor(knex:Knex) {
         this._knex = knex;
+    }
+
+    public async emptyCache() {
+        if (DEBUG_OUTPUT) {
+            console.log(`ResolverCache is truncating the database table...`);
+        }
+
+        await this._knex('ResolverCache').truncate();
+    }
+
+    public async pruneCache(): Promise<number> {
+        if (DEBUG_OUTPUT) {
+            console.log(`ResolverCache is pruning the database table...`);
+        }
+
+        return await this._knex('ResolverCache').whereRaw('CURRENT_TIMESTAMP > expiry').delete();
     }
 
     public async load(hash: string): Promise<BudgetReportOutputGroup | null> {
@@ -28,32 +43,32 @@ export class ResolverCache {
 
         const result = await query;
         return result.length < 1 ? null : {
-            period: result[0].data[0],
-            keys: result[0].data[1],
-            rows: result[0].data[2],
-            cacheKeys: null
+            cacheKeys: result[0].data[0],
+            period: result[0].data[1],
+            keys: result[0].data[2],
+            rows: result[0].data[3],
         };
     }
 
-    public async store(outputGroups: BudgetReportOutputGroup[]) {
+    public async store(outputGroups: BudgetReportOutputGroup[], expirationInMinutes:number = 240) {
         if (DEBUG_OUTPUT) {
             console.log(`ResolverCache is storing ${outputGroups.length} output groups: `, outputGroups);
         }
 
         for (const group of outputGroups.filter(g => g.cacheKeys !== null)) {
-            const hash = await this._calculateHash(group.cacheKeys as CacheKeys);
+            const hash = await this.calculateHash(group.cacheKeys as CacheKeys);
             await this._knex('ResolverCache')
                 .insert({
                     hash,
-                    expiry: this._knex.raw('CURRENT_TIMESTAMP + interval \'4 hours\''),
-                    data: JSON.stringify([group.period, group.keys, group.rows])
+                    expiry: this._knex.raw('CURRENT_TIMESTAMP + interval \'' + expirationInMinutes + ' minutes\''),
+                    data: JSON.stringify([group.cacheKeys, group.period, group.keys, group.rows])
                 })
                 .onConflict('hash')
                 .merge();
         }
     }
 
-    private async _calculateHash(keys: CacheKeys) {
+    public async calculateHash(keys: CacheKeys) {
         if (!this._h64) {
             this._h64 = (await xxhash()).h64ToString;
         }
