bc6956f68ce560011d7ffd4805fd1dbcbdab0dee
liberuum <liberom@proton.me>
2023-03-15 14:32:32 +0000

    add query tests

--- src/modules/BudgetStatement/query.spec.ts
+++ src/modules/BudgetStatement/query.spec.ts
@@ -5,7 +5,7 @@ import defaultSettings from '../default.config';
 import { ApolloServer } from 'apollo-server-express';
 
 
-const queryData = {
+const budgetStatementsQuery = {
     query: `query BudgetStatements {
         budgetStatements {
             id
@@ -13,7 +13,32 @@ const queryData = {
             month
         }
     }`
-}
+};
+
+const bsWalletsQuery = {
+    query: `query BudgetStatementWallets {
+        budgetStatementWallets {
+          id
+          budgetStatementId
+          name
+        }
+    }`
+};
+
+let bsLineItemsQuery = {
+    query: `query BudgetStatementLineItems($filter: BudgetStatementLineItemFilter) {
+        budgetStatementLineItems(filter: $filter) {
+          id
+          budgetStatementWalletId
+          month
+        }
+      }`,
+    variables: {
+        filter: {
+            budgetStatementWalletId: 100
+        }
+    }
+} as any;
 
 let server: any, db: any;
 
@@ -21,15 +46,34 @@ beforeAll(async () => {
     db = new EcosystemDatabase(initKnex());
     const { typeDefs, resolvers } = await linkApiModules(db, defaultSettings)
     server = new ApolloServer({ typeDefs, resolvers, dataSources: () => ({ db }) })
-})
+});
 
 afterAll(async () => {
     await db.knex.destroy()
     await server.stop()
-})
+});
 
 it('fetches budgetStatements', async () => {
-    const response = await server.executeOperation(queryData)
+    const response = await server.executeOperation(budgetStatementsQuery)
     expect(response.errors).toBeUndefined()
     expect(response.data.budgetStatements.length).toBeGreaterThan(0)
-})
+});
+
+it('fetches bsWallets', async () => {
+    const response = await server.executeOperation(bsWalletsQuery)
+    expect(response.errors).toBeUndefined()
+    expect(response.data.budgetStatementWallets.length).toBeGreaterThan(0)
+});
+
+it('fetches bsLineItems', async () => {
+    const response1 = await server.executeOperation(bsLineItemsQuery)
+    bsLineItemsQuery.variables.filter = {
+        ...bsLineItemsQuery.variables.filter,
+        month: '2022-01',
+        position: 1,
+    }
+    const response2 = await server.executeOperation(bsLineItemsQuery);
+    expect(response2.errors).toBeDefined()
+    expect(response1.errors).toBeUndefined()
+    expect(response1.data.budgetStatementLineItems.length).toBeGreaterThan(0)
+}); 
