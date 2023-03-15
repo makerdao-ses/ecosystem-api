06cc8a490360d17a1a4bbf1ecaa3aa85b5ee0958
Jevans13 <Jack13evans@gmail.com>
2023-03-15 15:37:06 -0600

    Release 0.7 seed

--- seeds/09-ReleaseV7.js
+++ seeds/09-ReleaseV7.js
@@ -7,12 +7,11 @@
 export async function seed(knex) {
 
 
-await knex.raw(`UPDATE "BudgetStatementLineItem" as bsli
-SET "budgetCap" = 12000
+await knex.raw(`UPDATE "BudgetStatementLineItem" 
+SET "budgetCap" =  12000
 FROM "BudgetStatement" AS bs
 LEFT JOIN "BudgetStatementWallet" AS bsw ON bs.id = bsw."budgetStatementId"
-WHERE bs."ownerCode" = 'DEL' AND bsli."canonicalBudgetCategory" = 'CompensationAndBenefits'`);
-
-
+WHERE bsw.id = "BudgetStatementLineItem"."budgetStatementWalletId"
+AND bs."ownerCode" = 'DEL'`);
 
 }
