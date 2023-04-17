d3319b43b8c9fc0dfd3aa1d8bc7e38baf978cd32
liberuum <liiberom@proton.me>
2023-04-17 09:48:34 +0300

    add migration that cleans Delegates budgetStatements

---
+++ migrations/new/202304170930_release_202305.js
@@ -0,0 +1,14 @@
+//Up migration removes ownerCode and ownerId values for ownerType = "Delegates"
+export async function up(knex) {
+
+    // Set ownerCode and ownerId to null in all budgetStatements with ownerType = "Delegates"
+    await knex("BudgetStatement").update({ ownerCode: null, ownerId: null }).where({ ownerType: "Delegates" })
+
+
+}
+
+//Down migration reverts the up migration change
+export async function down(knex) {
+
+
+}
