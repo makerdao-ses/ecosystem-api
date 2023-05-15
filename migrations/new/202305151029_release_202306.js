41f906a777239b23a93ad7c265acd50d258d2d8b
liberuum <liberom@proton.me>
2023-05-15 11:04:07 +0100

    ad

---
+++ migrations/new/202305151029_release_202306.js
@@ -0,0 +1,37 @@
+//Up migration adds budgetId column to budgetStatementLineItem, CoreUnit and Snapshot account tables
+export async function up(knex) {
+
+    console.log("Adding budgetId column to budgetStatementLineItem, CoreUnit and SnapshotAccount tables...");
+
+    await knex.schema.alterTable('BudgetStatementLineItem', (table) => {
+        table.integer('budgetId');
+    });
+
+    await knex.schema.alterTable('CoreUnit', (table) => {
+        table.integer('budgetId').nullable();
+    });
+    
+    await knex.schema.alterTable('SnapshotAccount', (table) => {
+        table.integer('budgetId').nullable();
+    });
+}
+
+//Down migration reverts the up migration change
+export async function down(knex) {
+
+    console.log('Reverting the change that added budgetId columns to BudgetStatmentLineItem, CoreUnit and SnapshotAccount tables...');
+
+    await knex.schema.alterTable('BudgetStatementLineItem', (table) => {
+        table.dropColumn('budgetId');
+    });
+    
+    await knex.schema.alterTable('CoreUnit', (table) => {
+        table.dropColumn('budgetId');
+    });
+    
+    await knex.schema.alterTable('SnapshotAccount', (table) => {
+        table.dropColumn('budgetId');
+    });
+
+    
+}
