69b9a431651c57f6930d09c14e4905a05065bf48
liberuum <liberom@proton.me>
2023-05-29 16:30:40 +0100

    created migration that adds BudgetStatementLineItem.currency column

---
+++ migrations/new/202305291620_release_202206.js
@@ -0,0 +1,20 @@
+// Up migration adds currency column in BudgetStatementLineItem table
+
+export async function up(knex) {
+
+    console.log('Adding currency column in BudgetStatementLineItem table...');
+
+    await knex.schema.alterTable('BudgetStatementLineItem', (table) => {
+        table.string('currency').notNullable().defaultTo('DAI');
+    });
+};
+
+//Down migration reverts the up migration change
+export async function down(knex) {
+
+    console.log('Reverting the change that added currency column in BudgetStatementLineItem table...');
+
+    await knex.schema.alterTable('BudgetStatementLineItem', (table) => {
+        table.dropColumn('currency');
+    });
+};
