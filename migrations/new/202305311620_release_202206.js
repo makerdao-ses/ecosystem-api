df09a71c387d8361dd9630c14131aca5db9bd903
Jevans13 <Jack13evans@gmail.com>
2023-05-31 09:29:29 -0600

    Migration that adds Aligned Delegates Budget Owner

---
+++ migrations/new/202305311620_release_202206.js
@@ -0,0 +1,27 @@
+// Up migration adds BudgetType AlignedDelegates
+
+export async function up(knex) {
+
+    console.log('Adding BudgetOwner AlignedDelegates...');
+
+    await knex.raw(`ALTER TYPE "BudgetOwner" ADD VALUE 'AlignedDelegates'`);
+};
+
+//Down migration reverts the up migration change
+export async function down(knex) {
+
+    console.log('Reverting the change that added BudgetOwner AlignedDelegates...');
+
+    await knex.schema.raw(`
+   CREATE TYPE new_BudgetOwner AS ENUM ('CoreUnit', 'Delegates', 'SpecialPurposeFund', 'Project', 'Ecosystem Actor');
+ 
+   ALTER TABLE "BudgetStatement" ALTER COLUMN "ownerType" TYPE new_BudgetOwner 
+       USING "ownerType"::text::new_BudgetOwner;
+ 
+   DROP TYPE "BudgetOwner";
+ 
+   ALTER TYPE new_BudgetOwner RENAME TO "BudgetOwner";
+   
+ `);
+
+};
