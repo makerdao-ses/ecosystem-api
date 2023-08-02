3e708e1def326b2bb3d07166e8a0a7aba3ed8d16
Jevans13 <Jack13evans@gmail.com>
2023-08-02 16:39:00 +0300

    Removing unused DEL Core Unit

---
+++ migrations/new/202308020900_release_202309.js
@@ -0,0 +1,33 @@
+// Up migration: remove unused delegate Core Unit
+
+
+export async function up(knex) {
+
+    console.log('Removing DEL Core Unit...');
+
+    const request = await knex('CoreUnit').where('code', 'DEL');
+    const delId = request[0].id;
+
+    await knex('UserRole').where('resourceId', delId).del();
+    await knex('CoreUnit').where('code', 'DEL').del();
+}
+
+export async function down(knex) {
+
+  console.log('Re-adding DEL Core Unit...');
+
+  await knex('CoreUnit').insert({
+    code: 'DEL',
+    name: 'Recognised Delegates',
+    image: null,
+    category: null,
+    sentenceDescription: null,
+    paragraphDescription: null,
+    paragraphImage: null,
+    shortCode: 'DEL',
+    legacyBudgetStatementUrl: null,
+    budgetId: null,
+    type: 'Delegates'
+  });
+
+}
