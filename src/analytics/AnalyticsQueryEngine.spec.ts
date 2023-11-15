35e9658964eada9a4dcc34c809bbd72bc902cdc2
Frank <frank.pfeift@protonmail.com>
2023-11-15 09:36:17 +0100

    test: added granularity sums and value tests for queryengine

--- src/analytics/AnalyticsQueryEngine.spec.ts
+++ src/analytics/AnalyticsQueryEngine.spec.ts
@@ -8,6 +8,10 @@ import {
 } from "./AnalyticsQuery";
 import { AnalyticsQueryEngine } from "./AnalyticsQueryEngine";
 import { AnalyticsStore } from "./AnalyticsStore";
+import {
+  GroupedPeriodResult,
+  GroupedPeriodResults,
+} from "./AnalyticsDiscretizer.js";
 
 let knex: Knex;
 
@@ -22,15 +26,16 @@ beforeAll(async () => {
   const store = new AnalyticsStore(knex);
   await store.clearSeriesBySource(TEST_SOURCE);
 
+  // budget
   await store.addSeriesValues([
     {
-      start: new Date(),
+      start: new Date(2021, 0, 1),
       source: TEST_SOURCE,
       value: 10000,
       unit: "DAI",
       metric: AnalyticsMetric.Budget,
       dimensions: {
-        budget: AnalyticsPath.fromString("atlas/legacy/core-units/SES-001"),
+        budget: AnalyticsPath.fromString("atlas/legacy/core-units/PE-001"),
         category: AnalyticsPath.fromString(
           "atlas/headcount/CompensationAndBenefits/FrontEndEngineering",
         ),
@@ -38,7 +43,21 @@ beforeAll(async () => {
       },
     },
     {
-      start: new Date(),
+      start: new Date(2022, 0, 1),
+      source: TEST_SOURCE,
+      value: 10000,
+      unit: "DAI",
+      metric: AnalyticsMetric.Budget,
+      dimensions: {
+        budget: AnalyticsPath.fromString("atlas/legacy/core-units/PE-001"),
+        category: AnalyticsPath.fromString(
+          "atlas/headcount/CompensationAndBenefits/FrontEndEngineering",
+        ),
+        project: TEST_SOURCE,
+      },
+    },
+    {
+      start: new Date(2023, 0, 1),
       source: TEST_SOURCE,
       value: 15000,
       unit: "DAI",
@@ -72,25 +91,27 @@ beforeAll(async () => {
     },
   ]);
 
-  await store.addSeriesValue({
-    start: new Date(2023, 0, 1),
-    source: TEST_SOURCE,
-    value: 5.8,
-    metric: AnalyticsMetric.FTEs,
-    dimensions: {
-      project: TEST_SOURCE,
+  // add fte's
+  await store.addSeriesValues([
+    {
+      start: new Date(2023, 0, 1),
+      source: TEST_SOURCE,
+      value: 5.8,
+      metric: AnalyticsMetric.FTEs,
+      dimensions: {
+        project: TEST_SOURCE,
+      },
     },
-  });
-
-  await store.addSeriesValue({
-    start: new Date(2023, 2, 1),
-    source: TEST_SOURCE,
-    value: -0.8,
-    metric: AnalyticsMetric.FTEs,
-    dimensions: {
-      project: TEST_SOURCE,
+    {
+      start: new Date(2023, 2, 1),
+      source: TEST_SOURCE,
+      value: -0.8,
+      metric: AnalyticsMetric.FTEs,
+      dimensions: {
+        project: TEST_SOURCE,
+      },
     },
-  });
+  ]);
 });
 
 afterAll(async () => {
@@ -117,7 +138,7 @@ it("should query records", async () => {
     currency: AnalyticsPath.fromString("DAI,MKR"),
     select: {
       budget: [
-        AnalyticsPath.fromString("atlas/legacy/core-units/SES-001"),
+        AnalyticsPath.fromString("atlas/legacy/core-units/PE-001"),
         AnalyticsPath.fromString("atlas/legacy/core-units/PE-001"),
       ],
       category: [AnalyticsPath.fromString("atlas/headcount")],
@@ -138,4 +159,111 @@ it("should query records", async () => {
     "atlas/legacy/core-units",
     "atlas/legacy/core-units",
   ]);
+});
+
+describe("total of different granularities", () => {
+  knex = initKnex();
+  const store = new AnalyticsStore(knex);
+  const engine = new AnalyticsQueryEngine(store);
+
+  let total = 0;
+
+  it("should return one row on total granularity", async () => {
+    const result = await getResultsForGranularity(AnalyticsGranularity.Total);
+    expect(result.length).toBe(1);
+    expect(result[0].rows[0].sum).toBe(25000);
+    total = result[0].rows[0].sum;
+  });
+
+  it("should correct sum up on annual granularity", async () => {
+    const result = await getResultsForGranularity(AnalyticsGranularity.Annual);
+    expect(result.length).toBe(2);
+    expect(getTotalSumOfResults(result)).toBe(total);
+    expect(result[result.length - 1].rows[0].sum).toBe(25000);
+  });
+
+  it("should correct sum up on semi annual granularity", async () => {
+    const result = await getResultsForGranularity(
+      AnalyticsGranularity.SemiAnnual,
+    );
+    expect(result.length).toBe(3);
+    expect(getTotalSumOfResults(result)).toBe(total);
+    expect(result[result.length - 1].rows[0].sum).toBe(25000);
+  });
+
+  it("should correct sum up on monthly granularity", async () => {
+    const result = await getResultsForGranularity(AnalyticsGranularity.Monthly);
+    expect(result.length).toBe(17);
+    expect(getTotalSumOfResults(result)).toBe(total);
+    expect(result[result.length - 1].rows[0].sum).toBe(25000);
+  });
+
+  it("should correct sum up on weekly granularity", async () => {
+    const result = await getResultsForGranularity(AnalyticsGranularity.Weekly);
+    expect(result.length).toBe(75);
+    expect(getTotalSumOfResults(result)).toBe(total);
+    expect(result[result.length - 1].rows[0].sum).toBe(25000);
+  });
+
+  it("should correct sum up on daily granularity", async () => {
+    const start = new Date("2021-12-30 00:00:00Z+0");
+    const end = new Date("2022-01-05 00:00:00Z+0");
+    const result = await getResultsForGranularity(
+      AnalyticsGranularity.Daily,
+      start,
+      end,
+    );
+    expect(result.length).toBe(6);
+    expect(result[result.length - 1].rows[0].sum).toBe(10000);
+  });
+
+  it("should correct sum up on hourly granularity", async () => {
+    const start = new Date("2021-12-31 23:00:00Z+0");
+    const end = new Date("2022-01-01 05:00:00Z+0");
+    const result = await getResultsForGranularity(
+      AnalyticsGranularity.Hourly,
+      start,
+      end,
+    );
+    expect(result.length).toBe(6);
+    expect(result[result.length - 1].rows[0].sum).toBe(10000);
+  });
+
+  const getResultsForGranularity = async (
+    granualarity: AnalyticsGranularity = AnalyticsGranularity.Annual,
+    start: Date = new Date("2022-01-01 00:00:00Z+0"),
+    end: Date = new Date("2023-06-01 00:00:00Z+0"),
+  ): Promise<GroupedPeriodResults> => {
+    const query: AnalyticsQuery = {
+      start,
+      end,
+      granularity: granualarity,
+      metrics: [AnalyticsMetric.Budget, AnalyticsMetric.Actuals],
+      currency: AnalyticsPath.fromString("DAI"),
+      select: {
+        budget: [AnalyticsPath.fromString("atlas/legacy/core-units/PE-001")],
+        category: [AnalyticsPath.fromString("atlas/headcount")],
+        project: [TEST_SOURCE],
+      },
+      lod: {
+        budget: 3,
+        category: 2,
+        project: 2,
+      },
+    };
+
+    const result = await engine.execute(query);
+    return result;
+  };
+
+  const getTotalSumOfResults = (result: GroupedPeriodResult[]) => {
+    let sum = 0;
+    result.forEach((result) => {
+      result.rows.forEach((row) => {
+        sum += row.value;
+      });
+    });
+
+    return sum;
+  };
 });
