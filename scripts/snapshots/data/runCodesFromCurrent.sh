ea0c35100d771ffe86e70e3cd0e377140b580383
Jevans13 <Jack13evans@gmail.com>
2024-02-02 08:57:02 -0300

    Fix snapshot run from current script - February error

--- scripts/snapshots/data/runCodesFromCurrent.sh
+++ scripts/snapshots/data/runCodesFromCurrent.sh
@@ -2,38 +2,8 @@
 
 # Define the list of codes
 codes=(
-  "core-units/GOV-001"
-  "core-units/SES-001"
-  "core-units/SAS-001"
-  "core-units/IS-001"
-  "core-units/RWF-001"
-  "core-units/GRO-001"
-  "core-units/ORA-001"
-  "core-units/COM-001"
-  "core-units/DAIF-001"
-  "core-units/SH-001"
-  "core-units/DUX-001"
-  "core-units/SNE-001"
-  "core-units/DIN-001"
-  "core-units/DECO-001"
-  "core-units/SF-001"
-  "core-units/TECH-001"
-  "core-units/EVENTS-001"
-  "core-units/CES-001"
-  "core-units/PE-001"
-  "core-units/RISK-001"
-  "delegates"
-  "spfs"
-  "ecosystem-actors/BA-LABS"
-  "ecosystem-actors/CHRONICLE"
-  "ecosystem-actors/DEWIZ"
-  "ecosystem-actors/JETSTREAM"
-  "ecosystem-actors/PHOENIX"
-  "ecosystem-actors/PULLUP"
-  "ecosystem-actors/SIDESTREAM"
-  "ecosystem-actors/TECHOPS"
+  
   "ecosystem-actors/VIRIDIAN"
-  "keepers"
 )
 
 
@@ -50,27 +20,32 @@ day_of_month=$(date -u +'%d')
 # Define months for PREVIOUS and CURRENT iterations
 if [ "$day_of_month" -lt 10 ]; then
   if [ "$current_month" -eq 1 ]; then
-    months=("12" "$current_month")
-    years=("$((current_year - 1))" "$current_year")
+    months=("11" "12")
+    years=("$((current_year - 1))" "$((current_year - 1))")
   else
-    if [ "$current_month" -eq 10 ]; then
-      months=("0$((current_month - 2))" "0$((current_month - 1))")
-      years=("$current_year" "$current_year")
+    if [ "$current_month" -eq 2 ]; then
+      months=("12" "0$((current_month - 1))")
+      years=("$((current_year - 1))" "$current_year")
     else
-      if [ "$current_month" -eq 11 ]; then
-        months=("0$((current_month - 2))" "$((current_month - 1))")
+      if [ "$current_month" -eq 10 ]; then
+        months=("0$((current_month - 2))" "0$((current_month - 1))")
         years=("$current_year" "$current_year")
       else
-        if [ "$current_month" -eq 12 ]; then
-          months=("$((current_month - 2))" "$((current_month - 1))")
+        if [ "$current_month" -eq 11 ]; then
+          months=("0$((current_month - 2))" "$((current_month - 1))")
           years=("$current_year" "$current_year")
         else
-          previous_month=$((${current_month#0} - 2))
-          previous_month="0$previous_month"
-          current_month=$((${current_month#0} - 1))
-          current_month="0$current_month"
-          months=("$previous_month" "$current_month")
-          years=("$current_year" "$current_year")
+          if [ "$current_month" -eq 12 ]; then
+            months=("$((current_month - 2))" "$((current_month - 1))")
+            years=("$current_year" "$current_year")
+          else
+            previous_month=$((${current_month#0} - 2))
+            previous_month="0$previous_month"
+            current_month=$((${current_month#0} - 1))
+            current_month="0$current_month"
+            months=("$previous_month" "$current_month")
+            years=("$current_year" "$current_year")
+          fi
         fi
       fi
     fi
