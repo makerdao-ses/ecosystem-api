ba304ce6c49c675404f06283a2e40fb48c37dcbd
liberuum <liberom@proton.me>
2024-03-11 10:43:27 +0000

    add logger

---
+++ logger.config.ts
@@ -0,0 +1,31 @@
+import { LoggerConfig } from './src/types';
+
+export const defaultLoggerConfig: LoggerConfig = {
+  // Filter by module name
+  moduleFilter: [],
+  // Filter by log prefix, e.g. adding
+  // `pref` to filter is going to correspond to showing `[PREF] ` logs
+  prefixFilter: [],
+  // Lowest printed log level of default logger
+  logLevel: 'info',
+  // Lowest printed log level of express logger
+  httpLogLevel: 'info',
+};
+
+export const debugLogConfig: LoggerConfig = {
+  // Filter by module name
+  moduleFilter: [],
+  // Filter by log prefix, e.g. adding
+  // `pref` to filter is going to correspond to showing `[PERF]` logs
+  prefixFilter: [],
+  // Lowest printed log level of default logger
+  logLevel: 'debug',
+  // Lowest printed log level of express logger
+  httpLogLevel: 'debug',
+};
+
+function getLoggerConfig(debug: boolean): LoggerConfig {
+  return (debug ? debugLogConfig : defaultLoggerConfig);
+}
+
+export const loggerConfig = getLoggerConfig(!!process.env.DEBUG);
