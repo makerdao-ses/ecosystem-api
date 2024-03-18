486deab58e33d74aec838ba0b5e19fc9c8c16fd7
liberuum <liberom@proton.me>
2024-03-18 13:39:57 +0000

    added no error memoize fn

--- src/utils/logWrapper.ts
+++ src/utils/logWrapper.ts
@@ -1,4 +1,5 @@
 import { getChildLogger } from "../logger.js"
+import memize from 'memize';
 
 export const measureQueryPerformance = async (queryName: string, moduleName: string, knexQuery: any) => {
     const logger = getChildLogger({}, { moduleName });
@@ -14,3 +15,19 @@ export const measureQueryPerformance = async (queryName: string, moduleName: str
 
     return result
 };
+
+// export const measureQueryPerformance = memize(async function (queryName: string, moduleName: string, knexQuery: any) {
+//     const logger = getChildLogger({}, { moduleName });
+
+//     const start = Date.now(); // Start timing
+//     const result = await knexQuery;
+//     const end = Date.now(); // End timing
+//     logger.info({
+//         executionTime: `${(end - start) / 1000}s`,
+//         query: knexQuery.toString(),
+//     },
+//         queryName);
+
+//     return result
+// });
+
