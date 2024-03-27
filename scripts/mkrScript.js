9e9b2b712d86fed73757fe2a99bdf2709b054a8c
Teep1 <Teep0x@proton.me>
2024-03-27 11:31:21 -0400

    Update mkrScript

--- scripts/mkrScript.js
+++ scripts/mkrScript.js
@@ -3,159 +3,150 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const connectionString = process.env.PG_CONNECTION_STRING;
-const pool = new Pool({
-  connectionString: connectionString,
-});
+const pool = new Pool({ connectionString });
 
 // Read data from the xlsx file
 const workbook = xlsx.readFile(process.env.MKR_SCRIPT_PATH);
 const mkrSheet = workbook.Sheets[workbook.SheetNames[0]];
-const mkrData = xlsx.utils.sheet_to_json(mkrSheet, {
-  raw: false,
-});
 const transferSheet = workbook.Sheets[workbook.SheetNames[1]];
-const transferData = xlsx.utils.sheet_to_json(transferSheet, {
-  raw: false,
-});
 
-//Initialise counters
+const getDataFromSheet = (sheet) =>
+  xlsx.utils.sheet_to_json(sheet, { raw: false });
+
+const mkrData = getDataFromSheet(mkrSheet);
+const transferData = getDataFromSheet(transferSheet);
+
+// Initialize counters
 let countTransfer = 0;
 let countMkr = 0;
 
-//Write data to the PostgreSQL database
+//// Write data to the PostgreSQL database
+async function processMkrRow(row) {
 
-mkrData.forEach((row) => {
-  //Transform the month value
-  const monthValue = new Date(row.month)
-    .toLocaleDateString("en-US")
-    .slice(0, 10);
+    const monthValue = new Date(row.month)
+      .toLocaleDateString("en-US")
+      .slice(0, 10);
 
-  //Retrieve the budget statement id
-  pool.query(
-    `SELECT id FROM "BudgetStatement" WHERE month  = '${monthValue}' AND "ownerCode" = '${row.ownerCode}'`,
-    (err, res) => {
-      if (err) {
-        console.log(err);
-      } else {
-        //Check for existence of Budget Statement
-        if (res.rows.length != 0) {
+    pool.query(
+      `SELECT id FROM "BudgetStatement" WHERE month = '${monthValue}' AND "ownerCode" = '${row.ownerCode}'`,
+      async (err, res) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        if (res.rows.length !== 0) {
           const budgetStatementIdVal = res.rows[0].id;
+          const vestDateIso = new Date(row.vestingDate)
+            .toLocaleDateString("en-US")
+            .slice(0, 10);
+          const comment = row.comments !== undefined ? row.comments : null;
+          const mkrAmount = parseFloat(row.mkrAmount);
+          const mkrAmountOld = parseFloat(row.mkrAmountOld);
 
-          const vestDate = new Date(row.vestingDate);
-          const vestDateIso = vestDate.toLocaleDateString("en-US").slice(0, 10);
-
-          let comment = null;
-
-          if (row.comments != undefined) {
-            comment = row.comments;
-            pool.query(
-              `WITH new_values (budgetStatementId, vestingDate, mkrAmount, mkrAmountOld, comments) AS (
-    VALUES (${budgetStatementIdVal}, '${vestDateIso}'::date, ${row.mkrAmount}, ${row.mkrAmountOld}, '${comment}')
-)
-INSERT INTO "BudgetStatementMkrVest" ("budgetStatementId", "vestingDate", "mkrAmount", "mkrAmountOld", "comments")
-SELECT *
-FROM new_values
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM "BudgetStatementMkrVest"
-    WHERE "budgetStatementId" = new_values.budgetStatementId
-    AND "vestingDate" = new_values.vestingDate
-    AND "mkrAmount" = new_values.mkrAmount
-    AND "mkrAmountOld" = new_values.mkrAmountOld
-);
-`,
-              (err, res) => {
-                countMkr++;
-              },
+          try {
+            const result = await pool.query(
+              `
+              WITH new_values AS (
+                SELECT $1::int AS "budgetStatementId", $2::date AS "vestingDate", $3::numeric AS "mkrAmount", $4::numeric AS "mkrAmountOld", $5 AS "comments"
+              )
+              INSERT INTO "BudgetStatementMkrVest" ("budgetStatementId", "vestingDate", "mkrAmount", "mkrAmountOld", "comments")
+              SELECT nv."budgetStatementId", nv."vestingDate", nv."mkrAmount", nv."mkrAmountOld", nv."comments"
+              FROM new_values nv
+              WHERE NOT EXISTS (
+                SELECT 1
+                FROM "BudgetStatementMkrVest"
+                WHERE "budgetStatementId" = nv."budgetStatementId"
+                AND "vestingDate" = nv."vestingDate"
+                AND "mkrAmount" = nv."mkrAmount"
+                AND "mkrAmountOld" = nv."mkrAmountOld"
+              );              
+              `,
+              [
+                budgetStatementIdVal,
+                vestDateIso,
+                mkrAmount,
+                mkrAmountOld,
+                comment,
+              ]
             );
-          } else {
-            pool.query(
-              `WITH new_values (budgetStatementId, vestingDate, mkrAmount, mkrAmountOld, comments) AS (
-    VALUES (${budgetStatementIdVal}, '${vestDateIso}'::date, ${row.mkrAmount}, ${row.mkrAmountOld}, ${comment})
-)
-INSERT INTO "BudgetStatementMkrVest" ("budgetStatementId", "vestingDate", "mkrAmount", "mkrAmountOld", "comments")
-SELECT *
-FROM new_values
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM "BudgetStatementMkrVest"
-    WHERE "budgetStatementId" = new_values.budgetStatementId
-    AND "vestingDate" = new_values.vestingDate
-    AND "mkrAmount" = new_values.mkrAmount
-    AND "mkrAmountOld" = new_values.mkrAmountOld
-);
-`,
-              (err, res) => {
-                countMkr++;
-              },
-            );
+          } catch (error) {
+            console.error(error);
           }
+          countMkr++
         }
       }
-    },
-  );
-});
+    );
+};
 
-transferData.forEach((row) => {
-  //Transform the month value
+
+async function processTransferData(row) {
   const monthValue = new Date(row.month)
     .toLocaleDateString("en-US")
     .slice(0, 10);
-  let walletAdd = row.address;
-  let walletAddLower = walletAdd.toLowerCase();
+  const walletAddLower = row.address.toLowerCase();
+  const { topupTransfer: requestAmount, currentBalance: walletBalance } = row;
 
-  const requestAmount = row.topupTransfer;
-  const walletBalance = row.currentBalance;
+  try {
+    const res = await pool.query(
+      `SELECT id FROM "BudgetStatement" WHERE month = '${monthValue}' AND "ownerCode" = '${row.ownerCode}'`,
+    );
 
-  //Retrieve the budget statement id
-  pool.query(
-    `SELECT id FROM "BudgetStatement" WHERE month  = '${monthValue}' AND "ownerCode" = '${row.ownerCode}'`,
-    (err, res) => {
-      if (err) {
-        console.log(err);
-      } else {
-        // Check for existence of Budget Statement
-        if (res.rows.length != 0) {
-          const budgetStatementIdVal = res.rows[0].id;
-          pool.query(
-            `SELECT id, name FROM "BudgetStatementWallet" WHERE "budgetStatementId" = ${budgetStatementIdVal} AND LOWER("address") = '${walletAddLower}'`,
-            (err1, res1) => {
-              if (res1.rows.length != 0) {
-                const walletId = res1.rows[0].id;
+    if (res.rows.length !== 0) {
+      const budgetStatementIdVal = res.rows[0].id;
 
-                // Insert new values -> then check already present values are up to date
-                pool.query(
-                  `
-                WITH new_values (budgetStatementWalletId, requestAmount, walletBalance) AS (                  
-                  VALUES (${walletId}, ${requestAmount}, ${walletBalance})
-                )
-                INSERT INTO "BudgetStatementTransferRequest" ("budgetStatementWalletId", "requestAmount", "walletBalance")
-                SELECT nv.budgetStatementWalletId, nv.requestAmount, nv.walletBalance
-                FROM new_values nv
-                WHERE NOT EXISTS (
-                  SELECT 1
-                  FROM "BudgetStatementTransferRequest" bs
-                  WHERE bs."budgetStatementWalletId" = nv.budgetStatementWalletId
-                );
-                
-                WITH new_values (budgetStatementWalletId, requestAmount, walletBalance) AS (                  
-                  VALUES (${walletId}, ${requestAmount}, ${walletBalance})
-                )
-                UPDATE "BudgetStatementTransferRequest" bs
-                SET "requestAmount" = nv.requestAmount,
-                    "walletBalance" = nv.walletBalance
-                FROM new_values nv
-                WHERE bs."budgetStatementWalletId" = nv.budgetStatementWalletId;
-                `,
-                  (err, res) => {
-                    countTransfer++;
-                  },
-                );
-              }
-            },
+      const res1 = await pool.query(
+        `SELECT id, name FROM "BudgetStatementWallet" WHERE "budgetStatementId" = ${budgetStatementIdVal} AND LOWER("address") = '${walletAddLower}'`,
+      );
+
+      if (res1.rows.length !== 0) {
+        const walletId = res1.rows[0].id;
+
+        await pool.query(
+          `
+          WITH new_values (budgetStatementWalletId, requestAmount, walletBalance) AS (
+            VALUES (${walletId}, ${requestAmount}, ${walletBalance})
+          )
+          INSERT INTO "BudgetStatementTransferRequest" ("budgetStatementWalletId", "requestAmount", "walletBalance")
+          SELECT nv.budgetStatementWalletId, nv.requestAmount, nv.walletBalance
+          FROM new_values nv
+          WHERE NOT EXISTS (
+            SELECT 1
+            FROM "BudgetStatementTransferRequest" bs
+            WHERE bs."budgetStatementWalletId" = nv.budgetStatementWalletId
           );
-        }
+
+          WITH new_values (budgetStatementWalletId, requestAmount, walletBalance) AS (
+            VALUES (${walletId}, ${requestAmount}, ${walletBalance})
+          )
+          UPDATE "BudgetStatementTransferRequest" bs
+          SET "requestAmount" = nv.requestAmount,
+              "walletBalance" = nv.walletBalance
+          FROM new_values nv
+          WHERE bs."budgetStatementWalletId" = nv.budgetStatementWalletId;
+          `,
+          (err, res) => {
+            if (err) console.error(err);
+    
+          },
+        );
       }
-    },
-  );
-});
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  countTransfer++;
+}
+
+// Process mkrData and transferData
+const mkrPromises = mkrData.map(processMkrRow);
+const transferPromises = transferData.map(processTransferData);
+
+Promise.all([...mkrPromises, ...transferPromises])
+  .then(() => {
+    console.log('Mkr Transfers:', countMkr);
+    console.log('Transfer Requests:', countTransfer);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
